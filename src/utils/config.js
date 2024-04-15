f2aeabc2af2142228243e88b203878d21c8a4b10
HoangDat0412 <hoang2811dat@gmail.com>
2024-04-15 14:47:47 +0700

    add ads

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,7 @@
 export const DOMAIN = "https://htad.id.vn/api/v1/films";
 export const LINKBE = 'https://htad.id.vn'
+
+
 export const TOKEN = "accessToken";
 export const USER_LOGIN = "USER_LOGIN";
 export const currentUser = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
